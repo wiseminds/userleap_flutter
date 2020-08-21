68e768cc118962f4da279f79ed04a67f85d7d47a
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2020-08-21 13:03:44 +0100

    completed android

--- example/android/app/src/main/kotlin/com/wiseminds/userleap_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/wiseminds/userleap_flutter_example/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.wiseminds.userleap_flutter_example
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:userleap_flutter/userleap_flutter.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized(); 
-   UserleapFlutter.configure('');
+  WidgetsFlutterBinding.ensureInitialized();
+  UserleapFlutter.configure('your environment id');
   runApp(MyApp());
 }
 
@@ -16,32 +14,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await UserleapFlutter.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -51,48 +26,78 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                  onPressed: () {
-                    UserleapFlutter.presentDebugSurvey();
-                  },
-                  child: Text('presentDebugSurvey')),
+        body: Builder(
+          builder: (context) => Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.presentDebugSurvey().then((value) =>
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('presentDebugSurvey')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.setUserIdentifier('ekeh.wisdom@gmail.com')
+                            .then((value) => Scaffold.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('setUserIdentifier')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.setEmailAddress('ekeh.wisdom@gmail.com')
+                            .then((value) => Scaffold.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('setEmailAddress')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.setVisitorAttribute('gender', 'male')
+                            .then((value) => Scaffold.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('setVisitorAttribute')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.track('login')
+                            .then((value) => Scaffold.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('track event')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        UserleapFlutter.logout().then((value) =>
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text(value?.toString() ?? ''))));
+                      },
+                      child: Text('logout')),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                  onPressed: () {
-                    UserleapFlutter.setUserIdentifier('yfbays7dbfsfgyb');
-                  },
-                  child: Text('setUserIdentifier')),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                  onPressed: () {
-                    UserleapFlutter.setEmailAddress('ekeh.wisdom@gmail.com');
-                  },
-                  child: Text('setEmailAddress')),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(
-                  onPressed: () {
-                    UserleapFlutter.setVisitorAttribute('gender', 'male');
-                  },
-                  child: Text('setVisitorAttribute')),
-            ),
-            
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Running on: $_platformVersion\n'),
-            ),
-          ],
+          ),
         ),
       ),
     );
